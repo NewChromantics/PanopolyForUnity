f5abcb160398a332f0c8aa0dc9c85558d3af76a4
Graham Reeves <graham@grahamreeves.com>
2021-04-27 17:39:00 +0100

    Depth to YUV now writes uv coords

--- DepthToYuvDepth.Frag.glsl
+++ DepthToYuvDepth.Frag.glsl
@@ -8,6 +8,7 @@ uniform float Encoded_ChromaRangeCount;
 uniform float Encoded_DepthMinMetres;
 uniform float Encoded_DepthMaxMetres;
 uniform float Encoded_LumaPingPong;
+#define ENCODE_LUMAPINGPONG	(Encoded_LumaPingPong>0.5)
 
 //	input
 uniform sampler2D DepthTexture;
@@ -126,14 +127,92 @@ void GetOutputUv_8_8_8(vec2 OutputImageUv,vec2 OutputImageSize,out vec2 PlaneSam
 }
 
 
-
-vec3 DepthToYuv(float Depth)
+bool IsOdd(int RangeIndex)
 {
+	return BitwiseAndOne(RangeIndex);
+	//return (RangeIndex&1)==1;
+}
+
+
+int GetUvRangeWidthHeight(int UvRangeCount)
+{
+	//	get WxH for this size
+	if (UvRangeCount < 2 * 2)	
+		return 1;
+	if (UvRangeCount <= 2 * 2)	return 2;
+	if (UvRangeCount <= 3 * 3)	return 3;
+	if (UvRangeCount <= 4 * 4)	return 4;
+	if (UvRangeCount <= 5 * 5)	return 5;
+	if (UvRangeCount <= 6 * 6)	return 6;
+	if (UvRangeCount <= 7 * 7)	return 7;
+	if (UvRangeCount <= 8 * 8)	return 8;
+	if (UvRangeCount <= 9 * 9)	return 9;
+	//if (UvRangeCount <= 10 * 10)
+		return 10;
+}
+
+vec2 GetChromaUvRange(int RangeIndex,int UvRangeCount)
+{
+	//	sqrt of range count
+	int RangeWidth = GetUvRangeWidthHeight(UvRangeCount);
+	float Width = float(RangeWidth);
+	float Height = float(RangeWidth);
+	float i = float(RangeIndex);
+	
+	float x = mod( i, Width );
+	float y = floor( i / Width );
+	float u = x / (Width - 1.0);
+	float v = y / (Height - 1.0);
+	return vec2(u,v);
+}
+
+
+vec3 DepthToYuv(float DepthMetres)
+{
+	//	normalise depth to 0..1 in valid range (and clamped)
+	float Depthf = Range01( Encoded_DepthMinMetres, Encoded_DepthMaxMetres, DepthMetres );
+	/*
+	auto DepthMin = Params.DepthMin;
+	auto DepthMax = Params.DepthMax;
+	auto UvRangeCount = Params.ChromaRangeCount;
+	
+	//	make our own YUV range count
+	uint8_t URange8s[MaxUvRangeCount];
+	uint8_t VRange8s[MaxUvRangeCount];
+	UvRangeCount = GetUvRanges8(UvRangeCount, URange8s, VRange8s, MaxUvRangeCount);
+*/
+	float RangeLengthMinus1 = max( Encoded_ChromaRangeCount-1.0, 1.0 );
+	//	get depth scaled to the 0...chunk-count range (up to range.99999, hence -1)
+	float DepthScaled = Depthf * RangeLengthMinus1;
+	
+	//	float Remain = DepthScaled - RangeIndex;
+	//float Remain = DepthScaled - floor(DepthScaled);	//	fractional/quant
+	float Remain = fract(DepthScaled);
+	//RangeIndex = min(RangeIndex, RangeLengthMinus1);	//	clamp range...
+
+	//	split value into Distance Chunk(uv range) and
+	//	it's high-resolution normalised/quantisied value inside (remain/fractional)
+	//float RangeIndex = floor(DepthScaled);
+	//float RangeIndex = DepthScaled - Remain;
+	//float RangeIndex = floor(Depthf * RangeLengthMinus1);
+	int RangeIndex = int(DepthScaled);
+	//float Remain = DepthScaled - RangeIndex;
+
+
+	//	make luma go 0-1 1-0 0-1 so luma image wont have edges for compression
+	if ( ENCODE_LUMAPINGPONG )
+		if ( IsOdd( RangeIndex ) )
+			Remain = 1.0 - Remain;
+
+	float Luma = Remain;
+	vec2 Chromauv = GetChromaUvRange( RangeIndex, int(Encoded_ChromaRangeCount) );
+	return vec3( Luma, Chromauv );
+/*
 	//	quick 1 chroma implementation
 	float Lumaf = Range01( Encoded_DepthMinMetres, Encoded_DepthMaxMetres, Depth );
-	
 	vec2 Chromauv = vec2(1,1);
 	return vec3( Lumaf, Chromauv );
+	*/
 }
 
 float GetDepthSample(vec2 uv)
